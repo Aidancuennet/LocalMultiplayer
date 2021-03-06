2a3d05de0fee4c39b69cc34347bf2622c0130ece
JeffAntonelli <antonellijeffrey@gmail.com>
2021-03-06 22:50:40 +0100

    Add field, player, goal, ball, physics, 1 player movement.

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private PlayerFoot foot;
+
+    private const float DeadZone = 0.1f;
+    private const float MoveSpeed = 5.0f;
+    private const float JumpSpeed = 10.0f;
+
+    private bool facingRight_ = true;
+    private bool jumpButtonDown_ = false;
+
+    void Start()
+    {
+        //  ChangeState(State.Jump);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpButtonDown_ = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveDir = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDir -= 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDir += 1.0f;
+        }
+
+         if (foot.FootContact > 0 && jumpButtonDown_)
+          {
+             Jump();
+          }
+           jumpButtonDown_ = false;
+
+        var vel = body.velocity;
+        body.velocity = new Vector2(MoveSpeed * moveDir, vel.y);
+    }
+    private void Jump()
+    {
+        var vel = body.velocity;
+        body.velocity = new Vector2(vel.x, JumpSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerFoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoot : MonoBehaviour
+{
+    private int _footContact = 0;
+
+    public int FootContact => _footContact;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            _footContact++;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            _footContact--;
+        }
+
+    }
+}
